09c2a4d686814d189dea71393c7e49df41f4538d
DELL <dksmanimendra0@gmail.com>
2024-08-08 15:39:24 +0530

    all done

---
+++ Thread-Pool/src/main/java/lk/ijse/MyRunnable.java
@@ -0,0 +1,21 @@
+package lk.ijse;
+
+public class MyRunnable implements Runnable{
+
+    public String name;
+
+    public MyRunnable(String name){
+        this.name=name;
+    }
+    @Override
+    public void run() {
+        System.out.println("Start Thread : "+name);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("End Thread : "+name);
+    }
+}
